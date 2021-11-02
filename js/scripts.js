b132145bb2e09a8605a3fcaecbd9ab372943db08
simon <sfacper.sansus3@gmail.com>
2021-11-02 09:41:01 +0100

    Inicializado ejercicios js
    - Creado js. Realizados ejercicios para alumnos

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Ejercicio 1
+let nombre = "Simón"
+console.log(nombre);
+
+//Ejercicios 2 y 3
+//nombre = "Simón Facorro Pérez"
+console.log(nombre);
+//al comentar nombre= simon facorro perez, lo que hace es realizar dos lecturas de la variable nombre = simón
+console.log(nombre);
+console.log(nombre);
+console.log(nombre);
+console.log(nombre);
+//vanse sumando as lecturas da variable
+
+let edad = "Su edad es:";
+console.log(edad);
+
+//Ejercicio 4
+let operador1 = 9;
+let operador2 = 13, operador3 = 2;
+let resultado = 0;
+resultado = operador1+operador2+operador3;
+console.log(resultado);
